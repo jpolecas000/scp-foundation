3cd918765c4d5c286b2598a7b971da22e21d390d
loz_fan2012 <jpolecas000@stu.waukesha.k12.wi.us>
2026-09-18 09:23:40 -0500

    Update pxt.json, tilemap.g.jres, tilemap.g.ts

---
+++ tilemap.g.ts
@@ -0,0 +1,70 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile2 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile5 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile7 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile8 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile6 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile4 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile3 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile9 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile10 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile1 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile11 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile12 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile13 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile14 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "Main Wall":
+            case "tile2":return tile2;
+            case "Main Wall-L2 Keycard":
+            case "tile5":return tile5;
+            case "Main Wall-L4 Keycard Slot":
+            case "tile7":return tile7;
+            case "Main Wall-O5 Keycard Slot":
+            case "tile8":return tile8;
+            case "Main Wall-L3 Keycard Slot":
+            case "tile6":return tile6;
+            case "Main Wall-L1 Keycard Slot":
+            case "tile4":return tile4;
+            case "Main Wall-L0 Keycard Slot":
+            case "tile3":return tile3;
+            case "Main Wall-Brodcast TV":
+            case "tile9":return tile9;
+            case "Main Wall-Brodcast TV-Hacked":
+            case "tile10":return tile10;
+            case "Main_Tile floor":
+            case "tile1":return tile1;
+            case "Heavy Door":
+            case "tile11":return tile11;
+            case "transparency16":return transparency16;
+            case "Main_Tile-Class-D Spawn":
+            case "tile12":return tile12;
+            case "Main_Tile floor- Scientific Spawn":
+            case "tile13":return tile13;
+            case "Main_Tile floor Security Spawn":
+            case "tile14":return tile14;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
