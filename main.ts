a83185c5730e18e15b7b8453bb5d369b983165d2
Turtles844 <dgruetzm000@stu.waukesha.k12.wi.us>
2026-09-21 15:49:15 -0500

    Update pxt.json, main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,44 @@
-
+let pClassD: Sprite = null
+let class_select = miniMenu.createMenu(
+miniMenu.createMenuItem("Class D"),
+miniMenu.createMenuItem("Chaos Insurgency"),
+miniMenu.createMenuItem("Security Force"),
+miniMenu.createMenuItem("Scientist"),
+miniMenu.createMenuItem("MTF")
+)
+tiles.setCurrentTilemap(tilemap`level1`)
+miniMenu.onButtonPressed(class_select, miniMenu.Button.A, function (selection, selectedIndex) {
+    if (selectedIndex == 0) {
+        miniMenu.close(class_select)
+        game.showLongText("Class D tutorial text", DialogLayout.Bottom)
+        pClassD = sprites.create(img`
+            . . . f f f f f f f . . . 
+            . . f d d d d d d d f . . 
+            . f d d 1 d d d 1 d d f . 
+            . f d 1 f d d d f 1 d f . 
+            . f d d d d d d d d d f . 
+            . f d d d f f f d d d f . 
+            . . f d d d d d d d f . . 
+            . f f 1 4 4 4 4 4 1 f f . 
+            f 4 f 4 1 4 4 4 1 4 f 4 f 
+            f 4 f 4 1 4 4 4 1 4 f 4 f 
+            f 6 f 4 1 4 4 4 1 4 f 6 f 
+            . f f 4 1 4 4 4 1 4 f f . 
+            . . f 1 4 4 4 4 4 1 f . . 
+            . . f f f f f f f f f . . 
+            . f b 4 4 f . f 4 4 b f . 
+            . . f f f . . . f f f . . 
+            `, SpriteKind.Player)
+        tiles.placeOnRandomTile(pClassD, assets.tile`Main_Tile-Class-D Spawn`)
+        controller.moveSprite(pClassD)
+        scene.cameraFollowSprite(pClassD)
+    } else if (selectedIndex == 1) {
+        game.showLongText("CI tutorial text", DialogLayout.Bottom)
+    } else if (selectedIndex == 2) {
+        game.showLongText("SF tutorial text", DialogLayout.Bottom)
+    } else if (selectedIndex == 3) {
+        game.showLongText("Scientist tutorial text", DialogLayout.Bottom)
+    } else if (selectedIndex == 4) {
+        game.showLongText("MTF tutorial text", DialogLayout.Bottom)
+    }
+})
--- tilemap.g.ts
+++ tilemap.g.ts
@@ -1,7 +1,37 @@
 // Auto-generated code. Do not edit.
 namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
-    export const tile2 = image.ofBuffer(hex``);
+    export const tile11 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile12 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile13 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile14 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile15 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile16 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile18 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile20 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile21 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile22 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile24 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile25 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile28 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile17 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
+    export const tile23 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
     export const tile5 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
@@ -19,22 +49,68 @@ namespace myTiles {
     //% fixedInstance jres blockIdentity=images._tile
     export const tile10 = image.ofBuffer(hex``);
     //% fixedInstance jres blockIdentity=images._tile
+    export const tile19 = image.ofBuffer(hex``);
+    //% fixedInstance jres blockIdentity=images._tile
     export const tile1 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile11 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const transparency16 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile12 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile13 = image.ofBuffer(hex``);
-    //% fixedInstance jres blockIdentity=images._tile
-    export const tile14 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1c001000000000000000000000000000000000000000101010101010100000000000131313131313131313131313000000170f0f0f0f0f0f0f1600000000130a130b130c130d130e131300000017131313131313131600000000131313131313131313131313000000001213151010101000000000001313131313131313131313130000000002090f070f0f03160000001313131313131315101213131300000000131313131313131600000013131313130913080f0713131300000014141414141411131600000013131313131313131313131313000000101010101010121316000000131510101010101010101012130000000f060f0f0f080f131600000013060f050f010f040f020f03130000001313131313131313160000001313131313131313131313131300000013131313131313131600000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`, img`
+.22222222222222...2222222...
+.2...............2.......2..
+.2...............2.......2..
+.2................2222222...
+22.......................2..
+2.......222...2..........2..
+2.....2.......2..2222222.2..
+2.............2..2222222.2..
+2.22222222222.2..........2..
+2.............2..........2..
+2.............2..........2..
+222222222222222.............
+............................
+............................
+............................
+............................
+`, [myTiles.transparency16,myTiles.tile5,myTiles.tile7,myTiles.tile8,myTiles.tile6,myTiles.tile4,myTiles.tile3,myTiles.tile9,myTiles.tile10,myTiles.tile11,myTiles.tile12,myTiles.tile13,myTiles.tile14,myTiles.tile15,myTiles.tile16,myTiles.tile17,myTiles.tile20,myTiles.tile21,myTiles.tile22,myTiles.tile23,myTiles.tile24,myTiles.tile25,myTiles.tile28,myTiles.tile1], TileScale.Sixteen);
+        }
+        return null;
+    })
 
     helpers._registerFactory("tile", function(name: string) {
         switch(helpers.stringTrim(name)) {
-            case "Main Wall":
-            case "tile2":return tile2;
+            case "Heavy Door":
+            case "tile11":return tile11;
+            case "transparency16":return transparency16;
+            case "Main_Tile-Class-D Spawn":
+            case "tile12":return tile12;
+            case "Main_Tile floor- Scientific Spawn":
+            case "tile13":return tile13;
+            case "Main_Tile floor Security Spawn":
+            case "tile14":return tile14;
+            case "Chaos Insurgency spawn":
+            case "tile15":return tile15;
+            case "MTF spawn":
+            case "tile16":return tile16;
+            case "myTile0":
+            case "tile18":return tile18;
+            case "myTile2":
+            case "tile20":return tile20;
+            case "myTile3":
+            case "tile21":return tile21;
+            case "myTile4":
+            case "tile22":return tile22;
+            case "myTile6":
+            case "tile24":return tile24;
+            case "myTile7":
+            case "tile25":return tile25;
+            case "myTile10":
+            case "tile28":return tile28;
+            case "main wall":
+            case "tile17":return tile17;
+            case "temporary main floor":
+            case "tile23":return tile23;
             case "Main Wall-L2 Keycard":
             case "tile5":return tile5;
             case "Main Wall-L4 Keycard Slot":
@@ -51,17 +127,10 @@ namespace myTiles {
             case "tile9":return tile9;
             case "Main Wall-Brodcast TV-Hacked":
             case "tile10":return tile10;
-            case "Main_Tile floor":
+            case "noseeZone":
+            case "tile19":return tile19;
+            case "myTile1":
             case "tile1":return tile1;
-            case "Heavy Door":
-            case "tile11":return tile11;
-            case "transparency16":return transparency16;
-            case "Main_Tile-Class-D Spawn":
-            case "tile12":return tile12;
-            case "Main_Tile floor- Scientific Spawn":
-            case "tile13":return tile13;
-            case "Main_Tile floor Security Spawn":
-            case "tile14":return tile14;
         }
         return null;
     })
