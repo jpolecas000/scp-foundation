4f526aaca27c2235c52b1652b0d12df46ecd8b6c
loz_fan2012 <jpolecas000@stu.waukesha.k12.wi.us>
2026-09-21 19:35:13 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,37 @@
-let mySprite = sprites.create(img`
-    . . . . . . f f f f . . . . . . 
-    . . . . f f f 2 2 f f f . . . . 
-    . . . f f f 2 2 2 2 f f f . . . 
-    . . f f f e e e e e e f f f . . 
-    . . f f e 2 2 2 2 2 2 e e f . . 
-    . . f e 2 f f f f f f 2 e f . . 
-    . . f f f f e e e e f f f f . . 
-    . f f e f b f 4 4 f b f e f f . 
-    . f e e 4 1 f d d f 1 4 e e f . 
-    . . f e e d d d d d d e e f . . 
-    . . . f e e 4 4 4 4 e e f . . . 
-    . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-    . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-    . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-    . . . . . f f f f f f . . . . . 
-    . . . . . f f . . f f . . . . . 
-    `, SpriteKind.Player)
-controller.moveSprite(mySprite)
-tiles.setCurrentTilemap(tilemap`Seat map`)
-scene.cameraFollowSprite(mySprite)
+let mySprite: Sprite = null
+let myMenu = miniMenu.createMenu(
+miniMenu.createMenuItem("Class D"),
+miniMenu.createMenuItem("Security"),
+miniMenu.createMenuItem("MTF")
+)
+miniMenu.onButtonPressed(myMenu, miniMenu.Button.A, function (selection, selectedIndex) {
+    if (selectedIndex == 0) {
+        mySprite = sprites.create(img`
+            . . . . . . f f f f . . . . . . 
+            . . . . f f f 4 4 f f f . . . . 
+            . . . f f f 4 4 4 4 f f f . . . 
+            . . f f f e e e e e e f f f . . 
+            . . f f e 4 4 4 4 4 4 e e f . . 
+            . . f e 4 f f f f f f 4 e f . . 
+            . . f f f f e e e e f f f f . . 
+            . f f e f b f d d f b f e f f . 
+            . f e e d 1 f d d f 1 d e e f . 
+            . . f e e d d d d d d e e f . . 
+            . . . f e e d d d d e e f . . . 
+            . . e d f 4 4 4 4 4 4 f d e . . 
+            . . d d f 4 4 4 4 4 4 f d d . . 
+            . . d d f d d 5 5 d d f d d . . 
+            . . . . . f f f f f f . . . . . 
+            . . . . . f f . . f f . . . . . 
+            `, SpriteKind.Player)
+        controller.moveSprite(mySprite)
+        tiles.setCurrentTilemap(tilemap`Seat map`)
+        scene.cameraFollowSprite(mySprite)
+        tiles.placeOnRandomTile(mySprite, assets.tile`Main_Tile-Class-D Spawn`)
+    } else if (selectedIndex == 1) {
+    	
+    } else if (selectedIndex == 2) {
+    	
+    }
+    miniMenu.close(myMenu)
+})
