8b31b2fcbdd2a5da9187d41c6b3d3fdc89365d6e
loz_fan2012 <jpolecas000@stu.waukesha.k12.wi.us>
2026-09-25 14:54:22 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
+namespace SpriteKind {
+    export const NPC = SpriteKind.create()
+}
+/**
+ * O= classd
+ * 
+ * 1=security
+ */
+let myMenu2: Sprite = null
+let BrainList: number[] = []
+let mySprite2: Sprite = null
 let mySprite: Sprite = null
 let myMenu = miniMenu.createMenu(
 miniMenu.createMenuItem("Class D"),
@@ -29,9 +40,50 @@ miniMenu.onButtonPressed(myMenu, miniMenu.Button.A, function (selection, selecte
         scene.cameraFollowSprite(mySprite)
         tiles.placeOnRandomTile(mySprite, assets.tile`Main_Tile-Class-D Spawn`)
     } else if (selectedIndex == 1) {
-    	
+        mySprite2 = sprites.create(img`
+            . . . . . . f f f f . . . . . . 
+            . . . . f f f d d f f f . . . . 
+            . . . f f f d d d d f f f . . . 
+            . . f f f e e e e e e f f f . . 
+            . . f f e d d d d d d e e f . . 
+            . . f e d f f f f f f d e f . . 
+            . . f f f f e e e e f f f f . . 
+            . f f e f b f b b f b f e f f . 
+            . f e e b 1 f b b f 1 b e e f . 
+            . . f e e b b b b b b e e f . . 
+            . . . f e e b b b b e e f . . . 
+            . . e b f d d d d d d f b e . . 
+            . . b b f d d d d d d f b b . . 
+            . . b b f b b 5 5 b b f b b . . 
+            . . . . . f f f f f f . . . . . 
+            . . . . . f f . . f f . . . . . 
+            `, SpriteKind.NPC)
+        sprites.setDataNumber(mySprite2, "faction", 1)
+        sprites.setDataNumber(mySprite2, "health", 100)
+        BrainList = []
+        sprites.setDataNumber(mySprite2, "brainID", BrainList.length)
     } else if (selectedIndex == 2) {
-    	
+        myMenu2 = miniMenu.createMenu(
+        miniMenu.createMenuItem("Nu-7"),
+        miniMenu.createMenuItem("Beta-7"),
+        miniMenu.createMenuItem("Epsilon-11"),
+        miniMenu.createMenuItem("Alpha-1")
+        )
+        miniMenu.onButtonPressed(myMenu, miniMenu.Button.A, function (selection, selectedIndex) {
+            if (selectedIndex == 0) {
+            	
+            } else if (selectedIndex == 1) {
+            	
+            } else if (selectedIndex == 2) {
+            	
+            } else if (selectedIndex == 3) {
+            	
+            }
+            miniMenu.close(myMenu2)
+        })
     }
     miniMenu.close(myMenu)
+})
+game.onUpdateInterval(200, function () {
+	
 })
